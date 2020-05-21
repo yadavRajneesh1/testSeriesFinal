93d2782f6264d8cb8f55bb53bc7148694ec08e61
yadavRajneesh1 <rajneeshy568@gmail.com>
2020-05-21 19:49:17 +0530

    login fuctionality with their decorator property is added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from functools import wraps
+from flask import Flask, render_template, request, session, flash, url_for
 from flask_wtf import FlaskForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import redirect
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import InputRequired, Email
 import psycopg2
@@ -10,7 +13,6 @@ with open("config.json", "r") as f:
     params = json.load(f)['params']
 
 app.secret_key = params['secret_key']
-
 
 connection = psycopg2.connect(user=params['user'], password=params['password'],
                               host=params['host'], port=params['port'],
@@ -34,12 +36,25 @@ class Question(FlaskForm):
     submit = SubmitField('Submit')
 
 
+def login_required(fuct):
+    @wraps(fuct)
+    def wrap(*args, **kwargs):
+        if 'user_id' in session:
+            return fuct(*args, **kwargs)
+        else:
+            flash('You need to login first')
+            return redirect(url_for('login'))
+
+    return wrap
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("home.html")
 
 
 @app.route('/myinfo')
+@login_required
 def Navbar():
     return render_template("myinfo.html")
 
@@ -52,22 +67,54 @@ def signup():
         password = login.password.data
         print(name)
         print(password)
+        hassed_password = generate_password_hash(login.password.data, method='sha256')
         insert_query = '''INSERT INTO public."user"(
-        	 "UserEmail", "UserPassword")
-        	VALUES (%s,%s);'''
-        cursor.execute(insert_query, (name, password))
+        "UserEmail", "UserPassword")
+        VALUES (%s,%s);'''
+        cursor.execute(insert_query, (name, hassed_password))
         connection.commit()
         return render_template("thanks.html", form=login)
     return render_template("signup.html", form=login)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    log = Login()
+    if request.method == 'POST':
+        session.pop('username', None)
+        username = request.form['username']
+        password = request.form['password']
+        print(username)
+        print(password)
+        # userone = usernew.query.filter_by(UserEmail=username).first()
+        select_query = '''SELECT no, "UserEmail", "UserPassword"
+	FROM public."user" where "UserEmail"=%s;'''
+        cursor.execute(select_query, (username,))
+        user_record = cursor.fetchall()
+        print(user_record)
+        if user_record and check_password_hash(user_record[0][2], password):
+            session['user_id'] = user_record[0][1]
+            return render_template('home.html')
+        return '<h1>Please make sure your credentials your username or password or both are incorrect</h1>'
+
+    return render_template('login.html', form=log)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.clear()
+    flash("you are logout")
+    return render_template('home.html')
+
+
 @app.route('/question', methods=['GET', 'POST'])
+@login_required
 def question():
     quest = Question()
     if quest.validate_on_submit():
         insert_query = '''INSERT INTO public."Question"(
-                	 question, option1, option2, option3, option4)
-                	VALUES (%s,%s,%s,%s,%s);'''
+        question, option1, option2, option3, option4)
+        VALUES (%s,%s,%s,%s,%s);'''
         cursor.execute(insert_query, (
             quest.question.data, quest.option1.data, quest.option2.data, quest.option3.data, quest.option4.data))
         connection.commit()
